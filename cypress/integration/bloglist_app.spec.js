9b365e852672b8c377d84987ee3ada2c4ddf5092
JTTran <tranjt80>
2020-11-20 18:26:47 +0200

    part5 ex5.19 bloglist end to end testing step3

--- cypress/integration/bloglist_app.spec.js
+++ cypress/integration/bloglist_app.spec.js
@@ -40,6 +40,26 @@ describe('Blog app', function () {
       cy.get('html').should('not.contain', 'admin')
     })
   })
+
+  describe.only('When logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'root', password: 'sekret' })
+    })
+
+    it('A blog can be created', function () {
+      cy.contains('New blog').click()
+      cy.get('#title').type('a cypress test')
+      cy.get('#author').type('newbie tester')
+      cy.get('#url').type('www.test.fi')
+      cy.contains('create').click()
+
+      cy.contains('a cypress test')
+      cy.contains('newbie tester')
+      cy.contains('www.test.fi')
+    })
+  })
+
+
 })
 
 
